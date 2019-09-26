965bd3e9e3bece94825a11738eebe2aa400e2c26
EIA Stream - PolyU/IC <36807259+polyueia@users.noreply.github.com>
2019-09-26 19:05:16 +0800

    Add files via upload
    Support ArduinoPLC and U5CBoard

--- PLC/PLC.cpp
+++ PLC/PLC.cpp
@@ -3,23 +3,77 @@
 int Y_BUF[8];
 int X_BUF[8];
 
-//<<constructor>> setup the LED, make pin 13 an OUTPUT
-PLC::PLC(){
-    pinMode(Y0+Y_OFS, OUTPUT); //make that pin an OUTPUT
-    pinMode(Y1+Y_OFS, OUTPUT); //make that pin an OUTPUT
-    pinMode(Y2+Y_OFS, OUTPUT); //make that pin an OUTPUT
-    pinMode(Y3+Y_OFS, OUTPUT); //make that pin an OUTPUT
+int X_ADDR[10];
+int Y_ADDR[10];
 
-    pinMode(X0+X_OFS, INPUT); //make that pin an OUTPUT
-    pinMode(X1+X_OFS, INPUT); //make that pin an OUTPUT
-    pinMode(X2+X_OFS, INPUT); //make that pin an OUTPUT
-    pinMode(X3+X_OFS, INPUT); //make that pin an OUTPUT
-    pinMode(X4+X_OFS, INPUT); //make that pin an OUTPUT
+//<<constructor>> setup the LED, make pin 13 an OUTPUT
+void XInit()
+{
+    pinMode(X_ADDR[0], INPUT); //make that pin an OUTPUT
+    pinMode(X_ADDR[1], INPUT); //make that pin an OUTPUT
+    pinMode(X_ADDR[2], INPUT); //make that pin an OUTPUT
+    pinMode(X_ADDR[3], INPUT); //make that pin an OUTPUT
+    pinMode(X_ADDR[4], INPUT); //make that pin an OUTPUT
 }
+
+void YInit()
+{
+    pinMode(Y_ADDR[0], OUTPUT); //make that pin an OUTPUT
+    pinMode(Y_ADDR[1], OUTPUT); //make that pin an OUTPUT
+    pinMode(Y_ADDR[2], OUTPUT); //make that pin an OUTPUT
+    pinMode(Y_ADDR[3], OUTPUT); //make that pin an OUTPUT
+
+
+}
+
+
+PLC::PLC(){
+/* For SMC Training Kit */
+    X_ADDR[0]=2;
+    X_ADDR[1]=3;
+    X_ADDR[2]=4;
+    X_ADDR[3]=5;
+    X_ADDR[4]=6;
+     
+    Y_ADDR[0]=7;
+    Y_ADDR[1]=8;
+    Y_ADDR[2]=9;
+    Y_ADDR[3]=10;
+
+    XInit();
+    YInit(); 
+}
+
+void PLC::setDI(int AX0, int AX1, int AX2, int AX3, int AX4)
+{
+    X_ADDR[0]=AX0;
+    X_ADDR[1]=AX1;
+    X_ADDR[2]=AX2;
+    X_ADDR[3]=AX3;
+    X_ADDR[4]=AX4;
+    XInit();
+
+}
+
+void PLC::setDQ(int AY0, int AY1, int AY2, int AY3)
+{
+    Y_ADDR[0]=AY0;
+    Y_ADDR[1]=AY1;
+    Y_ADDR[2]=AY2;
+    Y_ADDR[3]=AY3;
+    YInit(); 
+}
+
 
 //<<destructor>>
 PLC::~PLC(){/*nothing to destruct*/}
 
+int PLC::VER()
+{
+   
+   return 100+PLC_VERSION;
+
+}
 
 void PLC::IOP()
 {
@@ -27,31 +81,31 @@ void PLC::IOP()
    int X2_BUF[8];
    int X3_BUF[8];
 
-   int i=0, value = 0;
+   int i;
+   int value;
    for (i=0; i < 4; i++)
-      {
-         value= Y_BUF[i];
-        if (value == 0) 
-          digitalWrite(i+Y_OFS,HIGH); //set the pin HIGH and thus turn LED on
+   {
+   	value= Y_BUF[i];
+	if (value == 0) 
+      	  digitalWrite(Y_ADDR[i],HIGH); //set the pin HIGH and thus turn LED on
         else
-          digitalWrite(i+Y_OFS,LOW); //set the pin HIGH and thus turn LED on
+      	  digitalWrite(Y_ADDR[i],LOW); //set the pin HIGH and thus turn LED on
+   }
 
-      }   
-
-
+   
    for (i=0; i < 5; i++)
      {
-        X1_BUF[i]= digitalRead(i+X_OFS);
+        X1_BUF[i]= digitalRead(X_ADDR[i]);
      }
    delay(5);
    for (i=0; i < 5; i++)
      {
-        X2_BUF[i]= digitalRead(i+X_OFS);
+        X2_BUF[i]= digitalRead(X_ADDR[i]);
      }
    delay(5);
    for (i=0; i < 5; i++)
      {
-        X3_BUF[i]= digitalRead(i+X_OFS);
+        X3_BUF[i]= digitalRead(X_ADDR[i]);
         if ((X1_BUF[i]+X2_BUF[i]+X3_BUF[i])>=2)
            X_BUF[i]=1;
         else
--- PLC/PLC.h
+++ PLC/PLC.h
@@ -7,7 +7,7 @@
 #include "WProgram.h"
 #endif
 
-#define X_OFS 2
+#define PLC_VERSION 3
 #define X0 0
 #define X1 1
 #define X2 2
@@ -15,18 +15,23 @@
 #define X4 4
 
 
-#define Y_OFS 7
 #define Y0 0
 #define Y1 1
 #define Y2 2
 #define Y3 3
+
+
 class PLC { 
 public:
-	PLC();
-	~PLC();
+    PLC();
+    ~PLC();
+
+    void setDI(int AX0, int AX1, int AX2, int AX3, int AX4);
+    void setDQ(int AY0, int AY1, int AY2, int AY3);
     void DQ(int addr, int value);
     int DI(int addr);
     void IOP();
+    int VER();
 };
 
 #endif
